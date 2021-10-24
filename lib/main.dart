8f47632a9621fac89c0dc04eb06a27cccb2544d8
Oneya4 <46073127+Oneya4@users.noreply.github.com>
2021-10-24 11:27:52 +0300

    Initial Create

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const FooderLich());
+}
+
+class FooderLich extends StatelessWidget {
+  const FooderLich({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
