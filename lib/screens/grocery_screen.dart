61527ff9111a16b5b0af112ea81438ca8cae3418
Oneya4 <46073127+Oneya4@users.noreply.github.com>
2021-11-27 23:32:08 +0300

    Update grocery_screen.dart

--- lib/screens/grocery_screen.dart
+++ lib/screens/grocery_screen.dart
@@ -3,7 +3,6 @@ import 'package:provider/provider.dart';
 
 import '../models/models.dart';
 import 'empty_grocery_screen.dart';
-import 'grocery_item_screen.dart';
 import 'grocery_list_screen.dart';
 
 class GroceryScreen extends StatelessWidget {
@@ -16,18 +15,7 @@ class GroceryScreen extends StatelessWidget {
         child: const Icon(Icons.add),
         onPressed: () {
           final manager = Provider.of<GroceryManager>(context, listen: false);
-          Navigator.push(
-            context,
-            MaterialPageRoute(
-              builder: (context) => GroceryItemScreen(
-                onCreate: (item) {
-                  manager.addItem(item);
-                  Navigator.pop(context);
-                },
-                onUpdate: (item) {},
-              ),
-            ),
-          );
+          manager.createNewItem();
         },
       ),
       body: buildGroceryScreen(),
